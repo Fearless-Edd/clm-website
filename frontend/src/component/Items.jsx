08cad40638e9e50765ebf7f7c44cb430c6edffd8
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-01-19 19:05:27 -0600

    Chart section

--- frontend/src/component/Items.jsx
+++ frontend/src/component/Items.jsx
@@ -3,9 +3,9 @@ import PrimaryBtn from "../component/PrimaryBtn";
 
 const Item = ({ image, text, title, btnText }) => (
   <div className="relative">
-    <div className="absolute inset-0 flex items-start justify-center text-white p-5 lg:p-2  overflow-hidden object-contain
+    <div className="absolute inset-0 flex items-start justify-center text-white p-5 lg:p-10  overflow-hidden object-contain
     ">
-      <div className="flex flex-col gap-5 lg:gap-0">
+      <div className="flex flex-col md:gap-5 lg:gap-10">
         <h1 className="text-3xl font-semibold">{title}</h1>
         <p className="text-lg ">{text}</p>
         <PrimaryBtn title={btnText} textColor="#1D2130" bgColor="#FFFFFF" />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import "../assets/font/global.css";
 import Navbar from "../component/Navbar";
 import Hero from "../component/Hero";
 import {
@@ -10,12 +11,15 @@ import {
   logo6,
   wwd,
   subimg,
+  sub2,
+  sub3,
 } from "../assets/images";
 import React from "react";
 import PrimaryBtn from "../component/PrimaryBtn";
 import Services from "../component/Services";
 import { FaCity } from "react-icons/fa";
 import Item from "../component/Items";
+// import ChartResults from "../component/ChartResults";
 
 const Home = () => {
   const servicesData = [
@@ -42,27 +46,27 @@ const Home = () => {
   ];
 
   const itemsData = [
-  {
-    image: subimg,
-    title: "Cross Sessions Easter Celebrations",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-    btnText: "Learn More"
-  },
-  {
-    image: subimg,
-    title: "ECO-Panthers",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-    btnText: "Coming Soon"
-  },
-  {
-    image: subimg,
-    title: "Monthly public awareness",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
-    btnText: "Learn More"
-  }
-];
+    {
+      image: subimg,
+      title: "Cross Sessions Easter Celebrations",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+      btnText: "Learn More",
+    },
+    {
+      image: sub2,
+      title: "ECO-Panthers",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+      btnText: "Coming Soon",
+    },
+    {
+      image: sub3,
+      title: "Monthly public awareness",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.",
+      btnText: "Learn More",
+    },
+  ];
 
-
+  const logos = [logo1, logo2, logo3, logo4, logo5, logo6];
   return (
     <div>
       <Navbar />
@@ -74,7 +78,7 @@ const Home = () => {
           <div className="w-20 h-1 bg-black md:w-1/4 mt-3"></div>
           <div className="flex flex-col gap-10">
             <p className="font-semibold text-xl lg:pb-10">Know About us</p>
-            <h1 className="font-bold text-2xl text-btnColor lg:pb-10 lg:text-4xl">
+            <h1 className="font-bold  text-btnColor lg:pb-10 lg:text-4xl">
               CLM Native prioritizes community sensitization
             </h1>
             <div className="lg:text-xl ">
@@ -89,7 +93,6 @@ const Home = () => {
                 tristique posuere.
               </p>
             </div>
-           
           </div>
         </div>
         <div className="flex justify-center items-center lg:w-1/2">
@@ -98,22 +101,19 @@ const Home = () => {
       </div>
 
       {/* Our Suppouters */}
-      <div className="px-5 md:px-16">
+      <div className="px-5 md:px-20 lg:py-10">
         <h1 className="font-semibold py-4 md:font-bold">Our Suppouters</h1>
         <div className="grid grid-cols-3 gap-5 lg:grid-cols-6">
-          <img src={logo1} alt="" />
-          <img src={logo2} alt="" />
-          <img src={logo3} alt="" />
-          <img src={logo4} alt="" />
-          <img src={logo5} alt="" />
-          <img src={logo6} alt="" />
+          {logos.map((logo, index) => (
+            <img key={index} src={logo} alt={`Logo ${index + 1}`} />
+          ))}
         </div>
       </div>
 
       {/* what we do  */}
-      <div className="px-5 py-5 md:px-16 lg:px-20 md:flex justify-center items-center gap-10 bg-foregrround ">
-        <div className="flex flex-col gap-3 ">
-          <div className="w-20 h-1 bg-black md:w-1/4 mt-3"></div>
+      <div className="px-10 py-5 md:px-16 lg:px-20 md:flex justify-evenly items-center gap-10 bg-foregrround ">
+        <div className="flex flex-col gap-3 lg:w-1/3 lg:flex-row">
+          <div className="w-20 h-1 bg-black md:w-1/ mt-3"></div>
           <div className="flex flex-col gap-2">
             <h2 className="">What We Do</h2>
             <h1 className="font-semibold text-xl">
@@ -123,20 +123,19 @@ const Home = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique.
             </p>
-          </div>
-
-          <div className="">
-            {servicesData.map((service, index) => (
-              <Services
-                key={index}
-                icon={service.icon}
-                title={service.title}
-                desc={service.desc}
-              />
-            ))}
+            <div className="">
+              {servicesData.map((service, index) => (
+                <Services
+                  key={index}
+                  icon={service.icon}
+                  title={service.title}
+                  desc={service.desc}
+                />
+              ))}
+            </div>
           </div>
         </div>
-        <div className="md:w-full h-auto">
+        <div className="md:w-fu h-auto">
           <img src={wwd} alt="" />
         </div>
       </div>
@@ -145,28 +144,44 @@ const Home = () => {
       <div className="px-5 py-5 md:px-16 lg:px-20">
         <div>
           <div className="md:flex gap-2">
-            <div className="w-20 h-1 bg-black md:w-1/4 mt-3"></div>
+            <div className="w-20 h-1 bg-black md:w-1/1 mt-3"></div>
             <div className="">
               <p>Projects We Have Done</p>
             </div>
           </div>
           <div>
-            <h1 className="font-semibold text-xl">
+            <h1 className="font-bold  text-btnColor lg:pb-10 lg:text-4xl">
               We are creating a place <br /> where children with special <br />{" "}
               needs can thrive
             </h1>
           </div>
         </div>
         <div className="pt-10  text-white grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-        {itemsData.map((item, index) => (
-      <Item
-        key={index}
-        image={item.image}
-        title={item.title}
-        text={item.text}
-        btnText={item.btnText}
-      />
-    ))}
+          {itemsData.map((item, index) => (
+            <Item
+              key={index}
+              image={item.image}
+              title={item.title}
+              text={item.text}
+              btnText={item.btnText}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Chart section  */}
+      <div>
+        <div className="bg-black text-white w-full h-auto px-5 py-5 md:px-16 lg:px-20">
+          <div className="w-1/2">
+            <h1 className="font-bold lg:text-4xl">
+              How we spend your <br/> donations and where it goes
+            </h1>
+            <p className="py-2">We understand that when you make a  donation, you want to know exactly where your money is going and we pledge to be transparent.</p>
+          </div>
+
+          <div>
+       
+          </div>
         </div>
       </div>
     </div>
